e2c7c2fdd43e3a37e71808f1697f923ddbbda0be
Ddevyash <vandiyashira@gmail.com>
2025-03-05 12:37:08 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+let changecolor=document.querySelector('.mytext');
+let changecolor2=document.querySelector('.mytext2');
+let changecolor3=document.querySelector('.mytext3');
+let changecolor4=document.querySelector('.mytext4');
+
+
+function changeColor(){
+    changecolor.style.color='white';
+    changecolor2.style.color='white';
+    changecolor3.style.color='white';
+    changecolor4.style.color='white';
+}
